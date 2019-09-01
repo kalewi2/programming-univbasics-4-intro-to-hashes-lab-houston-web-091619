8aaf30949eabf894a0163f6922159b826ffd53b5
Kim Lewis <kalewi2@gmail.com>
2019-09-01 16:04:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-
+  read_from_hash = Hash.new
+  read_from_hash[hash, key]
 end
 
 def update_counting_hash(hash, key)
