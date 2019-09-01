3fb170db8b7ee9610fb9867758e5969bf73ca9ca
Kim Lewis <kalewi2@gmail.com>
2019-09-01 16:20:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,6 +36,7 @@ end
 
 def update_counting_hash(hash, key)
   # given a hash and a key as parameters, return an updated hash
+  if 
     return hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   #if update_counting_hash
